08ef6acea7a46da40709f8c8b5ad17199fcb59c1
Michael Carroll <mjoecarroll@gmail.com>
2023-09-08 11:10:35 -0400

    Trying to just update lit version

--- src/tabbed-card-editor.ts
+++ src/tabbed-card-editor.ts
@@ -12,9 +12,10 @@ import {
   LovelaceCardEditor,
   LovelaceCard,
 } from "custom-card-helpers";
-import "/@material/mwc-tab";
-import "/@material/mwc-tab-bar";
-import "/@material/mwc-ripple";
+// import "@material/mwc-tab";
+// import "@material/mwc-tab-bar";
+// import "@material/mwc-ripple";
+// import * as rip from "@material/mwc-ripple"
 // import { ScopedRegistryHost } from "@lit-labs/scoped-registry-mixin";
 import { BoilerplateCardConfig } from "./types";
 import { customElement, state } from "lit/decorators.js";
